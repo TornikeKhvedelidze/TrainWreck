a8166c2a44a7de473ffb696c276d355368ff1b5b
Tornike <tokaxvedela@gmail.com>
2025-05-04 14:48:35 +0400

    Little achievements polish

--- Assets/_Tech/Achivements/AchievementCard.cs
+++ Assets/_Tech/Achivements/AchievementCard.cs
@@ -1,18 +1,14 @@
 using TMPro;
 using UnityEngine;
-using UnityEngine.UI;
 
 public class AchievementCard : MonoBehaviour
 {
     [SerializeField] private TMP_Text _nameText;
     [SerializeField] private TMP_Text _descriptionText;
-    [SerializeField] private Image _background;
 
     public void Initialization(AchievementInfo info)
     {
         _nameText.text = info.Name;
         _descriptionText.text = info.description;
-
-        _background.color = info.IsClaimed ? Color.yellow : info.IsCompleted ? Color.green : Color.white;
     }
 }
